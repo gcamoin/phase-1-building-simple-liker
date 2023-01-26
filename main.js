53d16da11fc81c1bbc6013d535eab1634fafa6b1
Garrett <gcamoin5@gmail.com>
2023-01-26 11:09:40 -0500

    commit tree f61cd8d15bde4d166998736ce60bd1ed5d7bf0ee

--- main.js
+++ main.js
@@ -3,23 +3,30 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.querySelector('#modal');
+const message = document.querySelector('#modal-message');
 
+let hearts = document.querySelectorAll(".like")
 
+for (let heart of hearts) {
+  heart.addEventListener("click", addLike)
+}
 
-
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
-
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
-  });
+function like(e) {
+  if (e.target.innerText === EMPTY_HEART) {
+    mimicServerCall()
+    .then(() => {
+      e.target.innerText = FULL_HEART;
+      e.target.classList.add('activated-heart');
+    })
+    .catch(err => {
+      modal.classList.toggle('hidden');
+      message.innerText = err;
+      setTimeout(() => modal.classList.toggle('hidden'), 5000);
+    })
+  }
+  if (e.target.innerText === FULL_HEART) {
+    e.target.innerText = EMPTY_HEART;
+    e.target.classList.toggle('activated-heart');
+  }
 }
